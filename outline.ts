243e819ee192a0d9fbe069bf1ac85caa01ea277e
Sutin <poly.dev2018@gmail.com>
2021-12-03 13:04:09 +0700

    added todo

---
+++ outline.ts
@@ -0,0 +1,15 @@
+// FIXME: Introduction GraphQL
+
+// TODO: Setup Apollo Server
+
+// TODO: Define TypesDefs & Resolver
+
+// TODO: Mock data for Example
+
+// TODO: Playground serving
+
+// TODO: Simple Queries
+
+// TODO: Simple Mutations
+
+// TODO: Simple Subscriptions
