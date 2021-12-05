61f97596856fca5bd817ba817444093b6947ce4e
Sutin <poly.dev2018@gmail.com>
2021-12-05 22:18:09 +0700

    added cors, body_paser

--- src/main.mjs
+++ src/main.mjs
@@ -4,11 +4,17 @@ import express from 'express';
 import http from 'http';
 import expressJwt from 'express-jwt'
 import { connected } from './mongodb.mjs'
-
+import cors from 'cors'
+import bodyParser from 'body-parser';
 import { typeDefs } from './typesDefs.mjs'
 import { resolvers } from './resolvers.mjs'
 async function startApolloServer(typeDefs, resolvers) {
     const app = express();
+    app.use(cors())
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+
+
     app.use(
         expressJwt({
             secret: 'SECRET',
